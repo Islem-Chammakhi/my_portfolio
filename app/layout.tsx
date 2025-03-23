0a7f8549a792b51b099698728fbabec1dc288ab6
islem <islemchammakhi@gmail.com>
2025-03-23 11:14:03 +0100

    fixe responsivity problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-gray-100 font-sans">
         <Navbar />
-        <div className="container pt-8 mx-auto lg:grid lg:grid-cols-3 lg:gap-4 lg:pt-[80px] lg:pb-[80px] flex flex-col">
-          <div className="sticky top-0 self-start flex justify-center">
+        <div className="container pt-8 mx-auto lg:mx-auto lg:grid lg:grid-cols-3 lg:gap-4 lg:pt-[80px] lg:pb-[80px] flex flex-col">
+          <div className="lg:sticky lg:top-0 lg:self-start  mx-auto flex justify-center">
             <Profile />
           </div>
-          <div className="lg:col-span-2 rounded-md pb-8 mt-4 md:mt-0">
+          <div className="lg:pl-16 lg:col-span-2 rounded-md pb-8 mt-4 md:mt-0">
             {children}
           </div>
         </div>
